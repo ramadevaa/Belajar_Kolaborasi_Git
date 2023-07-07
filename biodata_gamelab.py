407859e3d054b9292f49bccdd428151a2e0bdc5c
ramadevaa <101534452+ramadevaa@users.noreply.github.com>
2023-07-07 14:07:45 +0800

    Update biodata_gamelab.py

--- biodata_gamelab.py
+++ biodata_gamelab.py
@@ -2,7 +2,7 @@ print ("=======================================")
 print ("Program Biodata Sederhana Dengan Python")
 print ("=======================================")
  
-nama = "Gamelab Indonesia"
+nama = "Gamelab Bali"
 hobi = "Membantu mengoptimalkan \n                skill programming"
 alamat = "Jalan Kalisombo No. 18"
 Kota = "Salatiga"
